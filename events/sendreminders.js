609437fcd36d4a7b38af979252497357ec2ee153
JeremyBearimy90 <68788055+JeremyBearimy90@users.noreply.github.com>
2021-11-07 22:19:20 +1100

    Everything seems to be working now

--- events/sendreminders.js
+++ events/sendreminders.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require('fs');
 
+
 module.exports = {
     name: "ready",
     once: true,
@@ -21,46 +22,34 @@ module.exports = {
                         
                         if (now_time - send_time == 0) {
                             var reactors = new Set();
-                            
+
                             text = item[0];
                             message_id = item[1];
                             channel_id = item[2];
                             send_channel = client.channels.cache.get(channel_id);
-                            console.log('match');
+
                             send_channel.messages.fetch(message_id).then(message => {
-                                reactors.add(1);
                                 message.reactions.cache.forEach(
                                     reaction => {reaction.users.fetch().then(
                                         user_list => {
-                                            user_list.forEach(user => {
-                                                console.log(user.id);
-                                                reactors.add(user.id);
-                                            })
+                                            user_list.forEach(
+                                                user => {
+                                                    if (!reactors.has(user.id)) {
+                                                        client.users.cache.get(user.id).send(text);
+                                                        reactors.add(user.id);
+                                                    }
+                                                }
+                                            )
                                         }
                                     )}
                                 )}
                              );
-                            
-                            reactors.forEach(user => {
-                                console.log(user);
-                            })
-                            
-                            // async fetchMessage(message_id) {
-                            //     let message = await send_channel.messages.fetch()
-                            // }
-
-
-
-                            // const text = message.contents;
-                            // console.log(message);
-                            // send_channel = client.channels.cache.get(channel_id);
-                            // send_channel.send(message);
                         };
                     });
 
                 }
             });
             
-        }, 1000 * 5)
+        }, 1000 * 60)
     },
 };
