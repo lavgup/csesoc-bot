c985e87dfff388a2691a1f797edae8290a51487b
Rifa Jamal <rifajamal@Rifas-MacBook-Air.local>
2021-12-11 13:45:45 +1100

    separated message logging stuff

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,12 +1,14 @@
 // import fs module where writeFile function is defined
 const fsLibrary = require('fs')
+const Discord = require('discord.js');
 
 module.exports = {
 
     name: "interactionCreate",
     once: false,
-    async execute(message) {
-
+    async execute(interaction) {
+        const message = interaction.message
+        const user = interaction.options.getUser('target');
         // if it is not a command, log the user command error
         if (!interaction.isCommand()) {
             await fsLibrary.writeFile('command_error_log.txt', "Logs command errors entered in the CSE discord channel", (error) => {
@@ -15,16 +17,17 @@ module.exports = {
                 if (error) throw err
             });
 
-            let log = `${Date.now()} - ${message.author.id} typed incorrect command "${message.content}" in "${message.channelId}"`
+            let log = `${Date.now()} - ${user.id} typed incorrect command "${message.content}" in "${message.channelId}"`
             
             // add new logged message into message_log file
             await fsLibrary.appendFile('command_error_log.txt', log)
         }
-
-        let log = `${Date.now()} - ${message.author.id} typed command "${message.content}" in "${message.channelId}"`
-            
-        // add new logged message into message_log file
-        await fsLibrary.appendFile('interaction_create_log.txt', log)
+        else {
+            let log = `${Date.now()} - ${user.id} typed command "${message.content}" in "${message.channelId}"`
+                
+            // add new logged message into message_log file
+            await fsLibrary.appendFile('interaction_create_log.txt', log)
+        }
     }
 
 };
