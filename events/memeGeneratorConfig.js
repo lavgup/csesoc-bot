54e7d46c6e7d02bbe94020ad2a048b7115b44072
Harry Zhang <harryzhang@192-168-1-122.tpgi.com.au>
2022-06-05 22:43:57 +1000

    Created the event to allow an admin to edit the Imgflip account

---
+++ events/memeGeneratorConfig.js
@@ -0,0 +1,21 @@
+const { Permissions } = require("discord.js");
+const fs = require('fs');
+
+module.exports = {
+    name: "messageCreate",
+    async execute(message) {
+        if (message.content.startsWith("$configMemeGenerator")) {
+            const messageContent = String(message.content);
+            const data = messageContent.split('\n');
+            
+            if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+                return await message.reply("You do not have permissions to use this command.");
+            }
+            if (data.length != 3) {
+                return await message.reply("Usage:\n$configMemeGenerator\nusername\npassword")
+            }
+
+            fs.writeFileSync("./config/memegenerator.json", JSON.stringify({ username: data[1], password: data[2] }, null, 4));
+        }
+    }
+}
