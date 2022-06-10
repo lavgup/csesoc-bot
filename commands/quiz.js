ee6c970a755d4b43ed28296baefa8ea9a7150ead
sschreyer <samschreyer360@gmail.com>
2022-06-10 16:10:19 +1000

    Setup quiz.js structure

--- commands/quiz.js
+++ commands/quiz.js
@@ -1,12 +1,52 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 
+//////////////////////////////////////////////
+////////// SETTING UP THE COMMANDS ///////////
+//////////////////////////////////////////////
+
+const commandCreateQuiz = new SlashCommandSubcommandBuilder()
+    .setName("help")
+    .setDescription("Get some information about the help command")
+    .addStringOption(option => option.setName("title").setDescription("Quiz title").setRequired(true));
+
+// base command
 const baseCommand = new SlashCommandBuilder()
     .setName("quiz")
     .setDescription("Setup a quiz!");
 
-async function handleInteraction (interaction) {
-    await interaction.reply("🏓 Pong!");
+//////////////////////////////////////////////
+/////////// HANDLING THE COMMANDS ////////////
+//////////////////////////////////////////////
+
+// handle the command
+/** @param {CommandInteraction} interaction */
+async function handleInteraction(interaction) {
+    // /** @type {DBFaq} */
+    // const faqStorage = global.faqStorage;
+
+    // figure out which command was called
+    const subcommand = interaction.options.getSubcommand(false);
+    switch (subcommand) {
+        case "get":
+            handleCreateQuiz(interaction);
+            break;
+        default:
+            await interaction.reply("Internal Error AHHHHHHH! CONTACT ME PLEASE!");
+    }
+}
+
+
+//////////////////////////////////////////////
+/////////// HANDLING THE COMMANDS ////////////
+//////////////////////////////////////////////
+
+/** 
+ * @param {CommandInteraction} interaction
+ */
+async function handleCreateQuiz(interaction) {
+    // TODO: implement
+    return;
 }
 
 
